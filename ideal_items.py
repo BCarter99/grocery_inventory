de57985630c49607b626245eb58bab070614f18c
Brandon Carter <brandon.d.carter99@gmail.com>
2021-05-24 15:40:56 -0400

    initial commit. Also created the ideal items/count list in ideal_items.py

---
+++ ideal_items.py
@@ -0,0 +1,11 @@
+def item_list():
+    ideal_items_list = {'bacon': 2, 'eggs': 60, 'sausage': 1, 'kielbasa': 1, 'meatballs': 1, 'juice': 1, 'chobani flip (cookie and cream)': 1,
+    'shredded cheese': 2, 'butter': 1, 'bread': 2, 'chicken nuggets': 1, 'Elaina\'s yogurt': 1, 'frosted flakes': 1, 'cheerios': 1,
+    'pop tarts': 1, 'mini cup mac and cheese': 3, 'prego meat sauce': 3, 'pasta sides': 3, 'hamburger helper': 2, 'elbow macaroni': 2,
+    'angel hair pasta': 2, 'fettucine': 2, 'minute rice': 2, 'uncle ben\'s rice sides': 4, 'rice pilaf': 3, 'instant potatoes': 2,
+    'corn': 4, 'shrimp': 1, 'pancake mix': 4, 'taco seasoning': 5, 'tortillas': 2, 'puffs': 2, 'water': 2, 'peanut butter crackers': 6,
+    'pringles': 2, 'doritos': 1, 'french toast (cinnamon)': 1, 'garlic bread': 1, 'waffles (cinnamon)': 1, 'mini pancakes': 1, 'ravioli': 1,
+    'fish sticks': 1, 'fries': 1, 'french bread pizza': 2, 'digorno pizza': 2, 'broccoli': 2, 'root beer': 24, 'goldfish': 2, 'muffins': 12,
+    'cosmic brownies': 1, 'm&m cookies': 1, 'ritz toasted chips': 1, 'animal crackers': 1, 'muffin bites': 5, 'brownie bites': 5,
+    'ritz crackers': 8, 'milanos': 2, 'oreos': 1, 'cheese-itz': 1, 'donuts': 2, 'ice cream (fudge swirl)': 1, 'steak': 1,
+    '4-pack boneless chicken': 2, '4-pack boneless pork chops': 2, 'ground beef': 2}
